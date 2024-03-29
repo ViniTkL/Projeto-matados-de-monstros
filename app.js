9b10d12939a2732a239c859e345f3de1e8155769
ViniTkL <vinimaxter150@gmail.com>
2024-03-29 10:51:42 -0400

    funcionalidades dos todos os botoões do jogo finalizada

--- app.js
+++ app.js
@@ -6,25 +6,76 @@ new Vue({
     playerDamage: 0,
     monsterDamage: 0,
     logs: [],
+    monsterLife: 100,
+    playerLife: 100,
    },
    methods: {
         attack(){
-            this.playerDamage = Math.round(Math.random()* (9 - 7) + 7);
-            console.log("Player",this.playerDamage);
-            this.monsterDamage = Math.round(Math.random()* (12 - 9) + 9);
-            console.log("Monstro",this.monsterDamage);
-            this.logs.push({player: `O player atacou com uma força de ${this.playerDamage}`, monster:`O monstro atacou com uma força de ${this.monsterDamage}`})
+            playerDamageDealt = this.palyerAttack()
+            monsterDamageDealt = this.monsterAttack()
+
+            this.damageDealt(playerDamageDealt, monsterDamageDealt);
+
+            this.logs.unshift({player: `O player atacou com uma força de ${playerDamageDealt}`, monster:`O monstro atacou com uma força de ${monsterDamageDealt}`});
         },
         specialAttack(){
+            
+            playerDamageDealt = this.playerSpecial()
+            monsterDamageDealt = this.monsterAttack()
 
+            this.damageDealt(playerDamageDealt, monsterDamageDealt);
+
+            this.logs.unshift({player: `O player atacou com uma força de ${playerDamageDealt}`, monster:`O monstro atacou com uma força de ${monsterDamageDealt}`});
+        },
+        monsterAttack(){
+            return this.monsterDamage = Math.round(Math.random()* (12 - 9) + 9);
+        },
+        palyerAttack(){
+            return this.playerDamage = Math.round(Math.random()* (9 - 7) + 7);
+        },
+        playerSpecial(){
+            return this.playerDamage = Math.round(Math.random()* (13 - 10) + 10);
+        },
+        damageDealt(playerDamage, monsterDamage){
+            this.monsterDamageReceived(playerDamage)
+            
+            this.playerDamageReceived(monsterDamage)
+        },
+        monsterDamageReceived(damageReceived){
+            monsterActualLife = this.monsterLife -= damageReceived
+            if(monsterActualLife <= 0){
+                this.monsterLife = 0;
+                this.isPlayerWinner = true;
+                this.gameEnd = true
+            }
+        },
+        playerDamageReceived(damageReceived){
+            playerActualLife = this.playerLife -= damageReceived
+            if(playerActualLife <= 0){
+               this.playerLife = 0;
+               this.isPlayerWinner = false;
+               this.gameEnd = true
+           }
         },
         heal(){
-
+            this.monsterDamage = Math.round(Math.random()* (8 - 6) + 8);
+            this.playerLife += Math.round(Math.random()* (12 - 9) + 9);
+            this.logs.unshift({player: `O player curou-se em ${this.playerDamage} de vida`, monster:`O monstro atacou com uma força de ${this.monsterDamage}`});
+        },
+        resetGame(){
+            this.gameEnd = false;
+            this.logs = []
+            this.playerLife = 100;
+            this.monsterLife = 100;
+        },
+        giveUp(){
+            this.gameEnd = true;
+            this.isPlayerWinner = false;
         }
    },
    computed: {
     gameLog(){
-        return
-    }
+        return this.logs
+    },
    }
 })
